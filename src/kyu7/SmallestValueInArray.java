da304677c1ee5061a6ab08504b44cf0381227185
stefan <stefanbayne@gmail.com>
2022-08-09 05:05:51 -0400

    Return smallest value in array.

---
+++ src/kyu7/SmallestValueInArray.java
@@ -0,0 +1,26 @@
+package kyu7;
+
+public class SmallestValueInArray {
+
+    public static void main(String[] args) {
+
+      /*
+      Write a function that can return the smallest value of an
+      array or the index of that value. The function's 2nd parameter
+      will tell whether it should return the value or the index.
+      Assume the first parameter will always be an array filled with
+      at least 1 number and no duplicates. Assume the second parameter
+      will be a string holding one of two values: 'value' and 'index'.
+       */
+
+
+
+    }
+
+
+    public static int findSmallest( final int[] numbers, final String toReturn ) {
+        //TODO: Add solution here
+        return 0;
+    }
+
+}
