4ff4b64129320ead1bc6320cde2a2e7c429932a5
stefan <stefanbayne@gmail.com>
2022-08-17 13:23:40 -0400

    New comments.

--- src/kyu7/CollatzConjectureLength.java
+++ src/kyu7/CollatzConjectureLength.java
@@ -2,6 +2,12 @@ package kyu7;
 
 public class CollatzConjectureLength {
     public static void main(String[] args) {
+        /*
+        For any natural number (positive) if it is even, divide by 2.
+        If it is odd, multiply by 3 and add one.
+
+        Return the length of numbers.
+         */
         System.out.println(conjecture(20));
     }
 
